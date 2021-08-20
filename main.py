b2bc9a30c2f31b03d909c2899931ca1bdb1def60
Avnish327030 <anishyadav7045075175@gmail.com>
2021-08-20 03:35:22 -0700

    port removed

--- main.py
+++ main.py
@@ -34,12 +34,12 @@ def predictRoute():
     return jsonify(result)
 
 
-#port = int(os.getenv("PORT", 5001))
 if __name__=="__main__":
+    port = int(os.getenv("PORT"))
     clApp = ClientApp()
     host = '0.0.0.0'
     #app.run()
-    httpd = simple_server.make_server(host=host, app=app)
+    httpd = simple_server.make_server(host=host,port=port, app=app)
     #httpd = simple_server.make_server(host='127.0.0.1', port=5000, app=app)
     # print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
